26f837b883f4d8d1b6dd49dff903b20a6dd1957b
Jeff Cobb <jeffcobb@berkeley.edu>
2018-09-16 03:52:58 +0800

    Set per interface rp_filter to 0.

--- scripts/enable_multicasting_fast.sh
+++ scripts/enable_multicasting_fast.sh
@@ -6,6 +6,10 @@ echo "/proc/sys/net/ipv4/conf/p2p2/force_igmp_version " `cat /proc/sys/net/ipv4/
 echo "/proc/sys/net/ipv4/conf/p2p3/force_igmp_version " `cat /proc/sys/net/ipv4/conf/p2p3/force_igmp_version`" (should be 2)"
 echo "/proc/sys/net/ipv4/conf/p2p4/force_igmp_version " `cat /proc/sys/net/ipv4/conf/p2p4/force_igmp_version`" (should be 2)"
 echo "/proc/sys/net/ipv4/conf/all/rp_filter           " `cat /proc/sys/net/ipv4/conf/all/rp_filter`" (should be 0)"         
+echo "/proc/sys/net/ipv4/conf/p2p1/rp_filter          " `cat /proc/sys/net/ipv4/conf/p2p1/rp_filter`" (should be 0)"         
+echo "/proc/sys/net/ipv4/conf/p2p2/rp_filter          " `cat /proc/sys/net/ipv4/conf/p2p2/rp_filter`" (should be 0)"         
+echo "/proc/sys/net/ipv4/conf/p2p3/rp_filter          " `cat /proc/sys/net/ipv4/conf/p2p3/rp_filter`" (should be 0)"         
+echo "/proc/sys/net/ipv4/conf/p2p4/rp_filter          " `cat /proc/sys/net/ipv4/conf/p2p4/rp_filter`" (should be 0)"         
 echo "/proc/sys/net/ipv4/conf/all/arp_ignore          " `cat /proc/sys/net/ipv4/conf/all/arp_ignore`" (should be 1)"
 echo "/proc/sys/net/ipv4/conf/all/arp_announce        " `cat /proc/sys/net/ipv4/conf/all/arp_announce`" (should be 2)"
 echo " "
@@ -18,6 +22,10 @@ sudo echo 2 > /proc/sys/net/ipv4/conf/p2p3/force_igmp_version
 sudo echo 2 > /proc/sys/net/ipv4/conf/p2p4/force_igmp_version
 # Disable reverse path filter
 sudo echo 0 > /proc/sys/net/ipv4/conf/all/rp_filter
+sudo echo 0 > /proc/sys/net/ipv4/conf/p2p1/rp_filter
+sudo echo 0 > /proc/sys/net/ipv4/conf/p2p2/rp_filter
+sudo echo 0 > /proc/sys/net/ipv4/conf/p2p3/rp_filter
+sudo echo 0 > /proc/sys/net/ipv4/conf/p2p4/rp_filter
 # Disable the so called "ARP flux" 
 sudo echo 1 > /proc/sys/net/ipv4/conf/all/arp_ignore
 sudo echo 2 > /proc/sys/net/ipv4/conf/all/arp_announce
@@ -29,6 +37,10 @@ echo "/proc/sys/net/ipv4/conf/p2p2/force_igmp_version " `cat /proc/sys/net/ipv4/
 echo "/proc/sys/net/ipv4/conf/p2p3/force_igmp_version " `cat /proc/sys/net/ipv4/conf/p2p3/force_igmp_version`" (should be 2)"
 echo "/proc/sys/net/ipv4/conf/p2p4/force_igmp_version " `cat /proc/sys/net/ipv4/conf/p2p4/force_igmp_version`" (should be 2)"
 echo "/proc/sys/net/ipv4/conf/all/rp_filter           " `cat /proc/sys/net/ipv4/conf/all/rp_filter`" (should be 0)"         
+echo "/proc/sys/net/ipv4/conf/p2p1/rp_filter          " `cat /proc/sys/net/ipv4/conf/p2p1/rp_filter`" (should be 0)"         
+echo "/proc/sys/net/ipv4/conf/p2p2/rp_filter          " `cat /proc/sys/net/ipv4/conf/p2p2/rp_filter`" (should be 0)"         
+echo "/proc/sys/net/ipv4/conf/p2p3/rp_filter          " `cat /proc/sys/net/ipv4/conf/p2p3/rp_filter`" (should be 0)"         
+echo "/proc/sys/net/ipv4/conf/p2p4/rp_filter          " `cat /proc/sys/net/ipv4/conf/p2p4/rp_filter`" (should be 0)"         
 echo "/proc/sys/net/ipv4/conf/all/arp_ignore          " `cat /proc/sys/net/ipv4/conf/all/arp_ignore`" (should be 1)"
 echo "/proc/sys/net/ipv4/conf/all/arp_announce        " `cat /proc/sys/net/ipv4/conf/all/arp_announce`" (should be 2)"
 echo " "
