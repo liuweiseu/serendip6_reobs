92f3dc42f7c1d615a2cdd4addfc30dba354f0d5b
Wei Liu <liuwei_berkeley@berkeley.edu>
2022-03-14 19:51:23 +0000

    tiny changes.

--- src/s6_test.sh
+++ src/s6_test.sh
@@ -27,7 +27,7 @@ hashpipe -p serendip6 -I $instance   \
     -o VERS6GW=$VERS6GW                \
     -o RUNALWYS=1                      \
     -o MAXHITS=2048                    \
-	-o POWTHRSH=40					   \ 
+	-o POWTHRSH=40					   \
     -o BINDHOST=$bindhost              \
     -o BINDPORT=12345                  \
     -o GPUDEV=$gpudev                  \
